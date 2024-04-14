40024c1f8ef0eed365128551bf574d71fa9a947b
gant <xtang2828@gmail.com>
2024-04-13 21:24:15 -0700

    comment add to encoder_forward test

--- test/test_encoder_forward.c
+++ test/test_encoder_forward.c
@@ -3,7 +3,7 @@
 
 // define the vocab size and the dimension of each word
 #define V 1000
-#define C 64
+// #define C 64
 
 // define the maximum sequence length and batch size
 #define max_T 128
@@ -11,15 +11,18 @@
 
 
 // test function
-void encoder_forward(float* out, int* inp, float* wte, float* wpe, int B, int T, int C_) {
-    for (int b = 0; b < B; b++) {
-        for (int t = 0; t < T; t++) {
+// wte: word token embedding, of shape (vocab_size, C); wpe: word position embedding, of shape(T, C)
+// B: batch size; T: number of words; C: dimension of each word
+// inp of shape (B, T, C); out of shape(B, T, C)
+void encoder_forward(float* out, int* inp, float* wte, float* wpe, int B, int T, int C) {
+    for (int b = 0; b < B; b++) { // for each batch, which contains a word sequence
+        for (int t = 0; t < T; t++) { // for each sequence, which contains T words
             float* out_bt = out + b * T * C + t * C;
             int ix = inp[b * T + t];
-            float* wte_ix =  wte + ix * C;
-            float* wpe_t  = wpe + t * C;
+            float* wte_ix =  wte + ix * C; // 定位到对应的词嵌入向量
+            float* wpe_t  = wpe + t * C; // Position the positional embedding corresponding to the position
 
-            for (int i = 0; i < C; i++) {
+            for (int i = 0; i < C; i++) { // for each word, we add token embedding and position embedding
                 out_bt[i] = wte_ix[i] + wpe_t[i];
             }
         }
@@ -28,6 +31,9 @@ void encoder_forward(float* out, int* inp, float* wte, float* wpe, int B, int T,
 
 
 int main() {
+    int C = 64;
+    int B = 1;
+
     // Randomly generated input sequence (array of integers) representing token indices in the vocabulary
     int inp[max_T];
     for (int i = 0; i < max_T; i++) {
